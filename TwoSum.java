b6902a526a6aebcae1d11c03c43b318f9b8b25ab
zhangchong <liningdie>
2016-03-08 16:30:59 +0800

    *hashmap优化

--- TwoSum.java
+++ TwoSum.java
@@ -1,17 +1,22 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
-	 public int[] twoSum(int[] nums, int target) {
-		 int[] result = null;
-		    int len = nums.length;
-		    for(int i = 0;i < len;i++){
-		        for(int j = i + 1; j < len; j++){
-		            if(nums[i] + nums[j] == target && i != j ){
-		                result = new int[]{i,j};
-		                return  result;
-		            }
-		        }
-		    }
-		   return result;
-	    }
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> keeper = new HashMap<Integer,Integer>();
+		int len = nums.length;
+		int[] result = null;
+		for (int i = 0; i < len; i++) {
+			if(!keeper.containsKey(nums[i])){
+				keeper.put(target - nums[i],i);
+			}else{
+				result = new int[2];
+				result[0] = keeper.get(nums[i]);
+				result[1] = i;
+			}
+		}
+		return result;
+	}
 }
