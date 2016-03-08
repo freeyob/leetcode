5205dd5cb4e6948acbac9caec7970a65b00acf08
freeyob <space.cong@gmail.com>
2016-03-08 18:02:39 +0800

    +LengthOfLastWord

---
+++ LengthOfLastWord.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class LengthOfLastWord {
+	public int lengthOfLastWord(String s) {
+		char empty = ' ';
+		int result = 0;
+		s = s.trim();
+		int len = s.length();
+		if(len > 0){
+			for(int i = 0; i <  len; i++){
+				if(s.charAt(len - i -1)  ==  empty){
+						
+				}
+			}
+		}
+        return result;
+    }
+}
