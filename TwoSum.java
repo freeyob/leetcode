d893a7f3004fd42b929db6fefe844252a977f31d
zhangchong <liningdie>
2016-03-08 13:06:52 +0800

    *优化%15.95

--- TwoSum.java
+++ TwoSum.java
@@ -2,16 +2,16 @@ package leetcode;
 
 public class TwoSum {
 	 public int[] twoSum(int[] nums, int target) {
-	    	int[] result = null;
-	    	int len = nums.length;
-	        for(int i = 0;i < len;i++){
-	            for(int j = 0; j < len; j++){
-	                if(nums[i] + nums[j] == target && i != j ){
-	                	result = new int[]{i,j};
-	                    return  result;
-	                }
-	            }
-	        }
-	        return result;
+		 int[] result = null;
+		    int len = nums.length;
+		    for(int i = 0;i < len;i++){
+		        for(int j = i + 1; j < len; j++){
+		            if(nums[i] + nums[j] == target && i != j ){
+		                result = new int[]{i,j};
+		                return  result;
+		            }
+		        }
+		    }
+		   return result;
 	    }
 }
