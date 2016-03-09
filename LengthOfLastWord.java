42676ce473cf54d6b5d02b257db7222a7d55a046
freeyob <space.cong@gmail.com>
2016-03-09 09:22:45 +0800

    *finish LengthOfLastWord

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -7,10 +7,15 @@ public class LengthOfLastWord {
 		s = s.trim();
 		int len = s.length();
 		if(len > 0){
-			for(int i = 0; i <  len; i++){
+			int i = 0;
+			for(; i <  len; i++){
 				if(s.charAt(len - i -1)  ==  empty){
-						
+						result = i;
+						break;
 				}
+			}
+			if(i == len){
+				result = len;
 			}
 		}
         return result;
