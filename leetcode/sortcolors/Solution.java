9f6d2fa93282093a1894e9f62c3656f02ad3e2a7
xiaobao <xiaobao@xiaobaodeMBP.lan>
2017-01-02 08:27:22 +0800

    * sortcolors 快了一点

--- leetcode/sortcolors/Solution.java
+++ leetcode/sortcolors/Solution.java
@@ -1,30 +1,36 @@
 package leetcode.sortcolors;
 
-import java.util.ArrayList;
-
 /**
  * Created by xiaobao on 2017/1/1.
  */
 public class Solution {
-    public void sortColors(int[] nums) {
-        int len = nums.length;
-        ArrayList<Integer> red = new ArrayList();
-        ArrayList<Integer> white = new ArrayList();
-        ArrayList<Integer> blue = new ArrayList();
-        for(int num :nums){
-            if(num == 0){
-                red.add(num);
-            }else if(num == 1){
-                white.add(num);
-            }else if(num == 2){
-                blue.add(num);
-            }
-        }
-        red.addAll(white);
-        red.addAll(blue);
-        for(int i = 0; i <len;i++){
-           nums[i] = red.get(i);
-           System.out.println(nums[i]);
+    void sortColors(int[] nums) {
+           int redCount = 0;
+           int whiteCount = 0;
+           for(int num:nums){
+              if(num == 0){
+                  redCount++;
+              }else if(num == 1){
+                  whiteCount++;
+              }
+           }
+           int redAndWhiteCount = redCount + whiteCount;
+           for(int i = 0; i < nums.length;i++){
+               if(i < redCount){
+                   nums[i] =0;
+               }else if(i < redAndWhiteCount) {
+                   nums[i] = 1;
+               }else {
+                   nums[i] = 2;
+               }
+           }
+    }
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] testDada = new int[]{0,2,1,1,2,1,0,1,2,1,2,1,0,0,2};
+        solution.sortColors(testDada);
+        for (int num: testDada){
+            System.out.println(num);
         }
     }
 }
