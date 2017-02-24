ce5af9107d66b12204c3f0c3ae74e56bd7cb1875
cong <space.cong@gmail.com>
2017-02-24 22:30:28 +0800

    +增加提交邮箱 * 修改测试数据

--- leetcode/titletonumber/Solution.java
+++ leetcode/titletonumber/Solution.java
@@ -21,7 +21,7 @@ public class Solution {
     }
     public static void main(String[] args){
         Solution solution = new Solution();
-        int result = solution.titleToNumber("B");
+        int result = solution.titleToNumber("ZZ");
         System.out.print(result);
     }
 }
