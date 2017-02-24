30c93c1a0fde50ba504423a0bccf86e92830a54c
xiaobao <xiaobao@xiaobaodeMBP.lan>
2017-02-24 22:14:15 +0800

    + titletonumber

---
+++ leetcode/titletonumber/Solution.java
@@ -0,0 +1,27 @@
+package leetcode.titletonumber;
+
+/**
+ * Created by xiaobao on 2017/2/24.
+ */
+public class Solution {
+    public int titleToNumber(String s) {
+        int num = 0;
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            num = num + getWeight(s.length() - i) * (c - 64);
+        }
+        return num;
+    }
+    private int getWeight(int level){
+        int result = 1;
+        for(int i = 0; i < level - 1; i++){
+            result *= 26;
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int result = solution.titleToNumber("B");
+        System.out.print(result);
+    }
+}
